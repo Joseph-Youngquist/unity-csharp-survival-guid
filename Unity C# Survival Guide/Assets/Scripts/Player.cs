fc14ed75d7b230684ab2606af3a52c51cb290e04
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-17 16:43:13 -0400

    Solution for Challenge #6 - speed

--- Unity C# Survival Guide/Assets/Scripts/Player.cs
+++ Unity C# Survival Guide/Assets/Scripts/Player.cs
@@ -10,6 +10,10 @@ public class Player : MonoBehaviour
     // when the speed is 0 print "you need to speed up!"
     // you can't go below zero.
 
+    [SerializeField]
+    private int _speed = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _speed++;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && _speed > 0)
+        {
+            _speed--;
+        }
+
+        if (_speed > 20)
+        {
+            Debug.Log("Slow Down!");
+        }
+
+        if (_speed == 0)
+        {
+            Debug.Log("You need to speed up!");
+        }
+
     }
 }
