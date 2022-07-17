f97be533a0960f2cd0206b91a55c5a50972c70b1
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-17 16:37:14 -0400

    if challenge #6 - speed

--- Unity C# Survival Guide/Assets/Scripts/Player.cs
+++ Unity C# Survival Guide/Assets/Scripts/Player.cs
@@ -4,48 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Create a program that when you hit the space key, you  increment a score value. When that score value is greater
-    // than 50, you turn the cube green. At the start of the program, set the cube to red.
-
-    [SerializeField]
-    private GameObject _cube;
-
-    [SerializeField]
-    private int _score;
-
-    private Renderer _cubeRenderer;
-    private bool _colorHasChanged = false;
+    // Create a program that lets you increment the speed when you hit the "s" key
+    // and the "A" key decrements the speed.
+    // When the speed is greater than 20 then print "Slow down"
+    // when the speed is 0 print "you need to speed up!"
+    // you can't go below zero.
 
     // Start is called before the first frame update
     void Start()
     {
-        if (_cube == null)
-        {
-            Debug.LogError("Player::Start() - cube is null");
-        }
-
-        _cubeRenderer = _cube.GetComponent<Renderer>();
-
-        if (_cubeRenderer == null)
-        {
-            Debug.LogError("Player::Start - cube renderer is null");
-        }
-
-        _cubeRenderer.material.color = Color.red;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            _score += 10;
-        }
-
-        if (_score > 50 && _colorHasChanged == false)
-        {
-            _cubeRenderer.material.color = Color.green;
-            _colorHasChanged = true;
-        }
+        
     }
 }
