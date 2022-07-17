62dfd813c3bfe6fb9a25fe9c6b8926d773f09d5e
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-17 15:21:36 -0400

    basics of if syntax

--- Unity C# Survival Guide/Assets/Scripts/Player.cs
+++ Unity C# Survival Guide/Assets/Scripts/Player.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int myAge = 14;
+    private int _legalAgeToDrive = 16;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // if my age is greater than or equal to 16
+        // then print out you can drive.
+
+        if (myAge >= _legalAgeToDrive)
+        {
+            Debug.Log("You can drive!");
+        }
+        else
+        {
+            Debug.Log("You can't drive because you are not at or above the legal age to drive which is " + _legalAgeToDrive);
+        }
     }
 
     // Update is called once per frame
