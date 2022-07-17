a713640b3484619312e2166eec3589f3d272f4fc
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-17 16:54:26 -0400

    swtich challenge #1

--- Unity C# Survival Guide/Assets/Scripts/Player.cs
+++ Unity C# Survival Guide/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int points;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            points = 50;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            points = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            points = 0;
+        }
+
+        // based on the value of points, we print out.
+
+        // Challenge, convert the below into a switch statement.
+        if(points == 50)
+        {
+            Debug.Log("Points are 50!");
+        }
+        else if (points == 100)
+        {
+            Debug.Log("Points are 100!");
+        }
+        else
+        {
+            Debug.Log("You need 50 or 100 points to receive a message");
+        }
     }
 }
