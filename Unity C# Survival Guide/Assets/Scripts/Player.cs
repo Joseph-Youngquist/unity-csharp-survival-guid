77e752076c3eea93e244c1b57e87278130095ff6
Joe Youngquist <joseph.youngquist@gmail.com>
2022-07-17 15:23:31 -0400

    if challenge #1

--- Unity C# Survival Guide/Assets/Scripts/Player.cs
+++ Unity C# Survival Guide/Assets/Scripts/Player.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int myAge = 14;
-    private int _legalAgeToDrive = 16;
+    // Variable to hold points
 
     // Start is called before the first frame update
     void Start()
     {
-        // if my age is greater than or equal to 16
-        // then print out you can drive.
 
-        if (myAge >= _legalAgeToDrive)
-        {
-            Debug.Log("You can drive!");
-        }
-        else
-        {
-            Debug.Log("You can't drive because you are not at or above the legal age to drive which is " + _legalAgeToDrive);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if we hit the space key
+        // add 10 points.
+
+        if()
+        {
+            // add 10 points
+        }
     }
 }
